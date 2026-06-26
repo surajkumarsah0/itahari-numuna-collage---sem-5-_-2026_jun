98dbf9de7842db25c9b77638946d3b2c8760a753
NGR-Np <ngr112001@gmail.com>
2026-06-26 08:56:48 +0545

    day 7 - user create action

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -1,16 +1,23 @@
 'use server'
 
+import { createUserInDB } from "@/services/userServices";
+
 export async function registerUser(data: FormData) {
     const username = data.get('username')?.toString();
     const email = data.get('email')?.toString();
     const password = data.get('password')?.toString();
+    
 
     if (!username || !email || !password) {
         throw new Error('Missing required fields');
     }
-
-    // Perform registration logic here (e.g., save to database)
-    // For demonstration purposes, we'll just log the data
+    const newUser = {   
+    username,
+    email,
+    password
+  };
+  //database call to create user
+    await createUserInDB(newUser); 
     console.log('Registering user:', { username, email, password });
 
     // Return a success message or any other relevant information
